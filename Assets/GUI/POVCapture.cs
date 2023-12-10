86031c574d246feded1ed8c3da197ea0657b48be
Federico Mantovani <fettemanto@gmail.com>
2023-12-10 12:45:55 +0100

    Added webcam support in OCR

--- Assets/GUI/POVCapture.cs
+++ Assets/GUI/POVCapture.cs
@@ -4,154 +4,215 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.IO;
+using System.Linq;
 using MixedReality.Toolkit.UX;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TMPro;
- 
+using UnityEngine.Windows.WebCam;
+
 public class ScreenShot : MonoBehaviour
 {
-    string path;
-    string image_path;
-    private string endpoint = "<azure_ocr_api_endpoint>vision/v3.2/read/analyze";
-    private string apiKey = "<azure_ocr_api_key>";
-    private string getResultUrl;
-    public UnityEngine.UI.Image screenshotDisplay; 
-    public GameObject screenshotPanel;
-    public GameObject labelPanel;
-    //public UnityEngine.UI.Text labelText; create label
-    public TMP_Text labelText;
-    public PressableButton screenshotButton;
+	string path;
+	string image_path;
+	private string endpoint = "<azure_ocr_api_endpoint>vision/v3.2/read/analyze";
+	private string apiKey = "<azure_ocr_api_key>";
+	private string getResultUrl;
+	public UnityEngine.UI.Image screenshotDisplay;
+	public GameObject screenshotPanel;
 
-    private string patternLLNN = @"^[A-Za-z]{2}\d{2}$";
-    private string patternID = @"^(?:SBB )?(\d{3}-\d{2}-\d{3})$";
+	public GameObject labelPanel;
 
-    // Start is called before the first frame update
-    void Start()
-    { 
-        path = Application.temporaryCachePath + "\\";  
-        Debug.Log(path);
-        image_path = path + "capture.png";
-        screenshotPanel.SetActive(false);
-        
-    }
+	//public UnityEngine.UI.Text labelText; create label
+	public TMP_Text labelText;
+	public PressableButton screenshotButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private string patternLLNN = @"^[A-Za-z]{2}\d{2}$";
+	private string patternID = @"^(?:SBB )?(\d{3}-\d{2}-\d{3})$";
 
-    public void Capture() {
-        screenshotButton.enabled = false;
-        StartCoroutine(CaptureImage());
-    }
+	private PhotoCapture photoCaptureObject = null;
 
-    private IEnumerator CaptureImage()
-    { 
-        
-        ScreenCapture.CaptureScreenshot(image_path);
-        yield return new WaitForSeconds(1); 
+	// Start is called before the first frame update
+	void Start()
+	{
+		path = Application.temporaryCachePath + "\\";
+		Debug.Log(path);
+		image_path = path + "capture.png";
+		screenshotPanel.SetActive(false);
+	}
 
-        Texture2D screenshotTexture = new Texture2D(2, 2);  
-        byte[] imageData = File.ReadAllBytes(image_path);
-        screenshotTexture.LoadImage(imageData);
-        screenshotDisplay.sprite = Sprite.Create(screenshotTexture, new Rect(0, 0, screenshotTexture.width, screenshotTexture.height), new Vector2(0.5f, 0.5f));
-        screenshotPanel.SetActive(true);
-        
-        StartCoroutine(SendImageForAnalysis());
-    }
+	// Update is called once per frame
+	void Update()
+	{
+	}
 
-    private IEnumerator SendImageForAnalysis()
-    {
-        byte[] imageData = File.ReadAllBytes(image_path);
-        UnityWebRequest request = UnityWebRequest.Put(endpoint, imageData);
-        request.method = "POST";
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/octet-stream");
-        request.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+	void OnPhotoCaptureCreated(PhotoCapture captureObject)
+	{
+		UnityEngine.Debug.Log("Photo Capture Object Created");
+		photoCaptureObject = captureObject;
 
-        yield return request.SendWebRequest();
+		Resolution cameraResolution =
+			PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
 
-        if (request.result != UnityWebRequest.Result.Success) 
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            getResultUrl = request.GetResponseHeaders()["Operation-Location"];
-            yield return new WaitForSeconds(0.1f);
-            StartCoroutine(GetAnalysisResults(getResultUrl));
-        }
-    }
+		CameraParameters c = new CameraParameters();
+		c.hologramOpacity = 0.0f;
+		c.cameraResolutionWidth = cameraResolution.width;
+		c.cameraResolutionHeight = cameraResolution.height;
+		c.pixelFormat = CapturePixelFormat.BGRA32;
 
-    private IEnumerator GetAnalysisResults(string getResultUrl)
-    {
-        bool succeeded = false;
-        while (!succeeded) {
-            UnityWebRequest request = UnityWebRequest.Get(getResultUrl);
-            request.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+		captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
+	}
 
-            yield return request.SendWebRequest(); 
+	void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+	{
+		photoCaptureObject.Dispose();
+		photoCaptureObject = null;
+	}
 
-            if (request.result != UnityWebRequest.Result.Success) 
-            {
-                Debug.Log(request.error);
-            }
-            else
-            {
-                string response = request.downloadHandler.text;
-                var data = JObject.Parse(response);
-                string status = data["status"].Value<string>();
-                Debug.Log("Status: " + status);
+	private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+	{
+		if (result.success)
+		{
+			photoCaptureObject.TakePhotoAsync(image_path, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
+		}
+		else
+		{
+			Debug.LogError("Unable to start photo mode!");
+		}
+	}
 
-                if (status == "succeeded")
-                {
-                    succeeded = true;
-                    screenshotButton.enabled = true;
-                    screenshotPanel.SetActive(false);
-                    Debug.Log("Analysis succeeded.");
-                    Debug.Log(response);
-                    string plaqueLabel = "n/a";
-                    string SBBID = "n/a";
-                    
-                    var readResults = data["analyzeResult"]["readResults"];
-                    foreach (var readResult in readResults)
-                    {
-                        var lines = readResult["lines"];
-                        foreach (var line in lines)
-                        {
-                            string text = line["text"].Value<string>();
-                            Debug.Log("Extracted Text: " + text);
-                            Match matchPlaqueLabel = Regex.Match(text, patternLLNN);
-                            Match matchSBBID = Regex.Match(text, patternID);
-                            if (matchPlaqueLabel.Success) {
-                                plaqueLabel = matchPlaqueLabel.Value;
-                            }
-                            if (matchSBBID.Success) {
-                                SBBID = matchSBBID.Groups[1].Value;
-                            }
-                        }
-                    }
+	void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+	{
+		if (result.success)
+		{
+			Debug.Log("Saved Photo to disk!");
+			ShowHUD();
+			photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+		}
+		else
+		{
+			Debug.Log("Failed to save Photo to disk");
+		}
+	}
+
+	public void Capture()
+	{
+		screenshotButton.enabled = false;
+		
+		if (Application.isEditor)
+		{
+			ScreenCapture.CaptureScreenshot(image_path);
+			ShowHUD();
+		}
+		else
+		{
+			PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
+		}
+	}
+
+	private void ShowHUD()
+	{
+		Texture2D screenshotTexture = new Texture2D(2, 2);
+		byte[] imageData = File.ReadAllBytes(image_path);
+		screenshotTexture.LoadImage(imageData);
+		screenshotDisplay.sprite = Sprite.Create(screenshotTexture,
+			new Rect(0, 0, screenshotTexture.width, screenshotTexture.height), new Vector2(0.5f, 0.5f));
+		screenshotPanel.SetActive(true);
+
+		StartCoroutine(SendImageForAnalysis());
+	}
+
+	private IEnumerator SendImageForAnalysis()
+	{
+		byte[] imageData = File.ReadAllBytes(image_path);
+		UnityWebRequest request = UnityWebRequest.Put(endpoint, imageData);
+		request.method = "POST";
+		request.downloadHandler = new DownloadHandlerBuffer();
+		request.SetRequestHeader("Content-Type", "application/octet-stream");
+		request.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+
+		yield return request.SendWebRequest();
+
+		if (request.result != UnityWebRequest.Result.Success)
+		{
+			Debug.Log(request.error);
+		}
+		else
+		{
+			getResultUrl = request.GetResponseHeaders()["Operation-Location"];
+			yield return new WaitForSeconds(0.1f);
+			StartCoroutine(GetAnalysisResults(getResultUrl));
+		}
+	}
+
+	private IEnumerator GetAnalysisResults(string getResultUrl)
+	{
+		bool succeeded = false;
+		while (!succeeded)
+		{
+			UnityWebRequest request = UnityWebRequest.Get(getResultUrl);
+			request.SetRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
+
+			yield return request.SendWebRequest();
+
+			if (request.result != UnityWebRequest.Result.Success)
+			{
+				Debug.Log(request.error);
+			}
+			else
+			{
+				string response = request.downloadHandler.text;
+				var data = JObject.Parse(response);
+				string status = data["status"].Value<string>();
+				Debug.Log("Status: " + status);
+
+				if (status == "succeeded")
+				{
+					succeeded = true;
+					screenshotButton.enabled = true;
+					screenshotPanel.SetActive(false);
+					Debug.Log("Analysis succeeded.");
+					Debug.Log(response);
+					string plaqueLabel = "n/a";
+					string SBBID = "n/a";
+
+					var readResults = data["analyzeResult"]["readResults"];
+					foreach (var readResult in readResults)
+					{
+						var lines = readResult["lines"];
+						foreach (var line in lines)
+						{
+							string text = line["text"].Value<string>();
+							Debug.Log("Extracted Text: " + text);
+							Match matchPlaqueLabel = Regex.Match(text, patternLLNN);
+							Match matchSBBID = Regex.Match(text, patternID);
+							if (matchPlaqueLabel.Success)
+							{
+								plaqueLabel = matchPlaqueLabel.Value;
+							}
+
+							if (matchSBBID.Success)
+							{
+								SBBID = matchSBBID.Groups[1].Value;
+							}
+						}
+					}
 
 
-                    labelPanel.SetActive(true);
-                    labelText.text = $"{plaqueLabel} - SBB {SBBID}";
-            
-                }
-                else if (status == "running")
-                {
-                    Debug.Log("Analysis still running... retrying in 0.5 seconds.");
-                    yield return new WaitForSeconds(0.5f);
-                }
-                else
-                {
-                    Debug.LogError("Analysis failed or other status received.");
-                    yield break;
-                }
-            }
-        }
-        
-    }
-
+					labelPanel.SetActive(true);
+					labelText.text = $"{plaqueLabel} - SBB {SBBID}";
+				}
+				else if (status == "running")
+				{
+					Debug.Log("Analysis still running... retrying in 0.5 seconds.");
+					yield return new WaitForSeconds(0.5f);
+				}
+				else
+				{
+					Debug.LogError("Analysis failed or other status received.");
+					yield break;
+				}
+			}
+		}
+	}
 }
